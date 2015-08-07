9866e413caa7a55dc25f2e6dcca6f181791a741a
lucusCofe <liting68@163.com>
2015-08-07 17:13:10 +0800

    update

--- v2/apiAction/order.php
+++ v2/apiAction/order.php
@@ -18,19 +18,29 @@ function pay(){
 	//$input_data['channel']='alipay';//wx
 	//$input_data['amount']=101;
 	if (empty($input_data['channel'])) {
-	    json_result(null,'110','支付方式未选择或金额不正确');return;
+	    json_result(null,'110','支付方式未选择');return;
 	}
 	if (empty($input_data['encouterid'])) {
 	    json_result(null,'111','支付对象丢失');return;
 	}
 	$channel = strtolower($input_data['channel']);
-	$orderNo = $input_data['loginid'].time();
-	$orderNo = $channel=='alipay'?'01'.$orderNo:'02'.$orderNo;
-        $encouter = $db->getRow('shop',array('id'=>$input_data['encouterid']));
-        if($input_data['loginid']!=$encouter['login_id']){
+        $isoldOrder=false;//是否已存在订单
+        if ($db->getCount('order',array('encouter_id'=>$input_data['encouterid']))>0){//如果已存在订单则判断
+                $old_order=$db->getRow('order',array('encouter_id'=>$input_data['encouterid']));
+                if($old_order['paid']==1){
+                        json_result(null,'201','订单已支付');return;
+                }
+                $orderNo=$old_order['order_no'];
+                $isoldOrder=true;
+        }else{
+                $orderNo = $input_data['loginid'].time();
+                $orderNo = $channel=='alipay'?'01'.$orderNo:'02'.$orderNo;
+        }
+        $encouter = $db->getRow('encouter',array('id'=>$input_data['encouterid']));
+        if($input_data['loginid']!=$encouter['user_id']){
 	    json_result(null,'112','你的订单不存在,请核对');return;
         }
-	$shop = $db->getRow('shop',array('id'=>$encouter['shopid']));
+	$shop = $db->getRow('shop',array('id'=>$encouter['shop_id']));
 	$menus=array();
 	$menubody=$shop['title'];
 	$totalamount=0;
@@ -40,9 +50,9 @@ function pay(){
         $price+=empty($encouter['price2'])?0:$encouter['price2']*1;
         $menubody .= '('.$product.')';
         $totalamount=$price*100;
-        echo $totalamount;
 	//$extra 在使用某些渠道的时候，需要填入相应的参数，其它渠道则是 array() .具体见以下代码或者官网中的文档。其他渠道时可以传空值也可以不传。
 	$extra = array();
+        //sk_live_OJQEx4iDNUjsC0BuR7UdMbRd sk_test_SSm1OOvD8anLzLaHSOGmnzzP
 	\Pingpp\Pingpp::setApiKey('sk_test_SSm1OOvD8anLzLaHSOGmnzzP');
 	try {
 	    $ch = \Pingpp\Charge::create(
@@ -58,14 +68,18 @@ function pay(){
 	            "app"       => array("id" => "app_rLSuDGnvvj9S8Ouf")
 	        )
 	    );
-	    $order=array('user_id'=>$input_data['loginid'],'charge_id'=>$ch['id'],'time_created'=>$ch['created'],'paid'=>2,'channel'=>$ch['channel'],'order_no'=>$ch['order_no'],'amount'=>$ch['amount'],'subject'=>$ch['subject'],'body'=>$ch['body'],'description'=>$ch['description'],'created'=>date('Y-m-d H:i:s'));
-	    $orderid=$db->create('order', $order);
-	    for ($i=1;$i<=2;$i++){
-                if(!empty($encouter['product'.$i])){
-	    	$od=array('order_id'=>$orderid,'user_id'=>$input_data['loginid'],'shop_id'=>$shop['id'],'name'=>$encouter['product'.$i],'img'=>$encouter['product_img'.$i],'price'=>$encouter['price'.$i],'created'=>date("Y-m-d H:i:s"));
-	    	$db->create('order_detail', $od);
+                $order=array('encouter_id'=>$encouter['id'],'user_id'=>$input_data['loginid'],'shop_id'=>$encouter['shop_id'],'time_created'=>$ch['created'],'paid'=>2,'channel'=>$ch['channel'],'order_no'=>$ch['order_no'],'amount'=>floor($ch['amount']/100),'subject'=>$ch['subject'],'body'=>$ch['body'],'description'=>$ch['description'],'created'=>date('Y-m-d H:i:s'));
+                if($isoldOrder){
+                        $db->update('order', $order,array('id'=>$old_order['id']));
+                }else{
+                        $orderid=$db->create('order', $order);
+                        for ($i=1;$i<=2;$i++){
+                            if(!empty($encouter['product'.$i])){
+                            $od=array('order_id'=>$orderid,'user_id'=>$input_data['loginid'],'shop_id'=>$encouter['shop_id'],'name'=>$encouter['product'.$i],'img'=>$encouter['product_img'.$i],'price'=>$encouter['price'.$i],'created'=>date("Y-m-d H:i:s"));
+                            $db->create('order_detail', $od);
+                            }
+                        }
                 }
-	    }
 	    echo $ch;
 	} catch (\Pingpp\Error\Base $e) {
 	    header('Status: ' . $e->getHttpStatus());
@@ -91,10 +105,11 @@ function webhooks(){
                 $order_no=$event->data->object->order_no;//订单号
                 $amount=$event->data->object->amount;//订单金额
                 $time_paid=$event->data->object->time_paid;//支付时间戳
+                $circlefile=APP_DIR. '/upload/order_no.db';
                 if($paid){
-                        $orderCondition=array('charge_id'=>$chargeid,'order_no'=>$order_no);
+                        $orderCondition=array('order_no'=>$order_no);
                         $order = $db->getRow('order',$orderCondition);
-                        $db->update('order', array('paid'=>1,'time_paid'=>$time_paid) , $orderCondition);
+                        $db->update('order', array('charge_id'=>$chargeid,'paid'=>1,'time_paid'=>$time_paid) , $orderCondition);
                         $db->update('encouter',array('status'=>2),array('id'=>$order['encouter_id']));
                 }
                 header($_SERVER['SERVER_PROTOCOL'] . ' 200 OK');
@@ -107,20 +122,7 @@ function webhooks(){
                 header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
                 break;
         }
-//	if($input_data['type'] == 'charge.succeeded'&& $input_data['data']['object']['paid'] == true)
-//	{
-//		//TODO update database
-//                $order = array('paid'=>1,'time_paid'=>$input_data['data']['time_paid']);
-//                $db->update('order', $order , array('order_no'=>$input_data['data']['order_no']));
-//		http_response_code(200);// PHP 5.4 or greater
-//	
-//	}
-//	
-//	else if($input_data['type'] == 'refund.succeeded'&& $input_data['data']['object']['succeed'] == true)
-//	{
-//		//TODO update database
-//		http_response_code(200);// PHP 5.4 or greater
-//	}
+        
 	
 }
 
